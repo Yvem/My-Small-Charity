3f078513e6dea6c63c5523b7c954267a7700084f
YEJ <ye.jutard@gmail.com>
2011-01-03 21:44:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysmallcharity::Application.config.secret_token = '84aac59a918b2d7353d0a27a9b8aea2680673714ad263c2b55c2c06a0bb132c75636c7fdb66b4062fde93610fd86af739f81726a702a2dd3f5b1b953d8d09cef'
